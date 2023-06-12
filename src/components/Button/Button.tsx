d97c6011f3eaa77b79371f11e68753466813c295
refrain62 <nao_h13@hotmail.com>
2023-06-13 01:14:10 +0900

    click イベントと Actions

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,14 +2,24 @@ import './button.css'
 
 type Props = {
     children: React.ReactNode,
-    color?: string,
-    //color?: 'default' | 'primary' | 'danger',
-    size?: string,
-    // size?: 'base' | 'sm' | 'lg',
+    // color?: string,
+    color?: 'default' | 'primary' | 'danger',
+    // size?: string,
+    size?: 'base' | 'sm' | 'lg',
+    onClick?: () => void,
 };
 
-function Button({ children, color = 'default', size = 'base' }: Props) {
-    return <button className={`${color} ${size}`}>{children}</button>;
+function Button({
+    children,
+    color = 'default',
+    size = 'base',
+    onClick = () => console.log('click'),
+    }: Props) {
+    return (
+        <button className={`${color} ${size}`} onClick={onClick}>
+            {children}
+        </button>
+    );
 }
 
 export default Button;
