ea348030db97da4ec8c0032bdd506d40a16e5832
refrain62 <nao_h13@hotmail.com>
2023-06-12 22:50:12 +0900

    components 配下だけ検知するように変更

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,11 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+// components フォルダ下で stories.js/jsx/ts/tsx という名前が入ったファイルのみ自動検知できるように変更
+//  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: [
+    '../src/components/**/*.mdx',
+    '../src/components/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -12,13 +12,13 @@ export default meta;
 // HelloButtonのストーリーを追加
 type Story = StoryObj<typeof Button>
 
-// export const HelloButton: Story = {
-//     args: {
-//         children: 'Hello World',
-//     },
-// };
+export const HelloButton: Story = {
+    args: {
+        children: 'Hello World',
+    },
+};
 
 // render関数を利用して記述も可能
-export const HelloButton: Story = {
+export const HelloButton2: Story = {
     render: () => <Button>Hello World2</Button>,
 };
