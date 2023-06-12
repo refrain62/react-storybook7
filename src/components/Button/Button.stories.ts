44f6f22aa61fbb6e17cf0119f20db2d2f1f82952
refrain62 <nao_h13@hotmail.com>
2023-06-12 22:33:02 +0900

    Button コンポーネントのストーリーを追加

---
+++ src/components/Button/Button.stories.ts
@@ -0,0 +1,10 @@
+import type { Meta } from '@storybook/react';
+
+import Button from './Button';
+
+const meta = {
+    title: 'Button',
+    component: Button,
+} satisfies Meta<typeof Button>;
+
+export default meta;
